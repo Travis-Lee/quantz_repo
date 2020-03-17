a642d228f18d69e7914a7ccc819a90962298e93b
Zhang Yuzheng <me@zhanyuzheng.com>
2020-03-17 21:50:41 +0800

    IndexDaily: fix: fix unittest calling incorrect function name
    Signed-off-by: Zhang Yuzheng <me@zhanyuzheng.com>

--- test/trade_calendar_manager_test.py
+++ test/trade_calendar_manager_test.py
@@ -5,9 +5,9 @@ from quantz_repo.trade_calendar_manager import TradeCalendarManager
 
 class TradeCalendarManagerTest(TestCase):
     def test_getNextTradeOf(self):
-        self.assertTrue(TradeCalendarManager.nextTradeDateOf(
+        self.assertTrue(TradeCalendarManager.next_trade_date_of(
             exchange='SSE', date='20200228') == '20200302', 'Incorrect next trade date of %s' % ('20200228'))
-        self.assertFalse(TradeCalendarManager.nextTradeDateOf(
+        self.assertFalse(TradeCalendarManager.next_trade_date_of(
             exchange='SSE', date='20201231') == '20200302', 'Incorrect next trade date of %s' % ('20201231'))
-        self.assertFalse(TradeCalendarManager.nextTradeDateOf(
+        self.assertFalse(TradeCalendarManager.next_trade_date_of(
             exchange='SSE', date='20211231') == '20200302', 'Incorrect next trade date of %s' % ('20211231'))
