7a84f750fca26496c829828cd0485eca1e6b710d
Zhang Yuzheng <me@zhanyuzheng.com>
2021-03-27 21:17:02 +0800

    Cfg: feat: make configuration cached in ram
    缓存配置信息到内存,而不是每次都从磁盘读取
    
    Signed-off-by: Zhang Yuzheng <me@zhanyuzheng.com>

--- quantz_repo/utils/configuraion.py
+++ quantz_repo/utils/configuraion.py
@@ -1,5 +1,7 @@
 import json
 
+_quantz_config = None
+
 
 class QuantzCfg:
     def __init__(self, cfg: str = 'quantz_cfg.json'):
@@ -15,5 +17,15 @@ class QuantzCfg:
             raise Exception('%s not in %s' % (name, self.__cfg_path))
 
 
-def initialize_quantz_config(cfg_path: str = 'quantz_cfg.json'):
-    return QuantzCfg(cfg_path)
+def initialize_quantz_config(cfg_path: str = 'quantz_cfg.json', force: bool = False):
+    '''
+    初始化配置，其中是简单的 json 即可
+    {
+        "aaa":"BBB",
+        "xxx"：“YYY”
+    }
+    通过 config.aaa 即可访问
+    '''
+    if _quantz_config is None or force == True:
+        _config = QuantzCfg(cfg_path)
+    return _config
