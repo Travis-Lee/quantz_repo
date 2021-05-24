ccae6bd27b0a2f476ee63cd55d89fd136d2bd5fe
Zhang Yuzheng <me@zhanyuzheng.com>
2021-05-24 19:47:53 +0800

    Configuration: feat: move default config to home
    default Configuration file location: ~/.quantz/quantz_cfg.json
    
    Signed-off-by: Zhang Yuzheng <me@zhanyuzheng.com>

--- quantz_repo/utils/configuraion.py
+++ quantz_repo/utils/configuraion.py
@@ -1,13 +1,25 @@
 import json
+import os
+import sys
+from pathlib import Path
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+# Since Python3.3, 禁用了相对路径import
 
 _quantz_config = None
 
 
 class QuantzCfg:
-    def __init__(self, cfg: str = 'quantz_cfg.json'):
-        with open(cfg, 'r') as f:
-            self.__config = json.load(f)
-            self.__cfg_path = cfg
+    def __init__(self, cfg: str = '%s/.quantz/quantz_cfg.json' % Path.home()):
+        try:
+            with open(cfg, 'r') as f:
+                self.__config = json.load(f)
+                self.__cfg_path = cfg
+        except Exception as e:
+            from quantz_exception import QuantzException
+            raise QuantzException(
+                'Failed to read quantz config from %s cause %s' % (cfg, e))
 
     def __getattr__(self, name):
         print('get %s' % name)
@@ -17,8 +29,9 @@ class QuantzCfg:
             raise Exception('%s not in %s' % (name, self.__cfg_path))
 
 
-def initialize_quantz_config(cfg_path: str = 'quantz_cfg.json', force: bool = False):
+def initialize_quantz_config(cfg_path: str = '%s/.quantz/quantz_cfg.json' % Path.home(), force: bool = False):
     '''
+    默认配置文件:~/.quantz/quantz_cfg.json .
     初始化配置，其中是简单的 json 即可
     {
         "aaa":"BBB",
